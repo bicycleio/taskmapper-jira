b34cdfa33d27e83f019c64c7755babed1bd26d3b
Rafael George <george.rafael@gmail.com>
2012-07-10 22:01:06 -0400

    Remove unused code

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -2,34 +2,30 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe TaskMapper::Provider::Jira::Project do 
 
-  let(:tm) { TaskMapper.new(:jira, :username => 'testuser', :password => 'testuser', :url => 'http://testing.jira.com') }
+  let(:tm) { TaskMapper.new(:jira, :username => 'soaptester', :password => 'soaptester', :url => 'http://jira.atlassian.com') }
   let(:project_class) { TaskMapper::Provider::Jira::Project }
-  before(:each) do
-    @tm = TaskMapper.new(:jira, :username => 'testuser', :password => 'testuser', :url => 'http://testing.jira.com') 
-    @klass = TaskMapper::Provider::Jira::Project
-  end
 
   describe "Retrieving projects" do 
+    before(:each) do 
+      stub_post 'http://jira.atlassian.com/rpc/soap/jirasoapservice-v2', 'projects.xml'
+    end
+
     context "when #projects" do 
       subject { tm.projects } 
+      it { should be_an_instance_of Array }
     end
-  end
-
-  it "should be able to load all projects" do
-    @tm.projects.should be_an_instance_of(Array)
-    @tm.projects.first.should be_an_instance_of(@klass)
   end
 
   it "should be able to load all projects based on an array of id's" do 
     pending
-    @tm.projects([1]).should be_an_instance_of(Array)
-    @tm.projects.first.should be_an_instance_of(@klass)
-    @tm.projects.first.id.should == 1
+    tm.projects([1]).should be_an_instance_of(Array)
+    tm.projects.first.should be_an_instance_of(@klass)
+    tm.projects.first.id.should == 1
   end
 
   it "should be load all projects based on attributes" do 
     pending
-    projects = @tm.projects(:id => 1)
+    projects = tm.projects(:id => 1)
     projects.should be_an_instance_of(Array)
     projects.first.should be_an_instance_of(@klass)
     projects.first.id.should == 1
@@ -37,14 +33,14 @@ describe TaskMapper::Provider::Jira::Project do
 
   it "should be able to load a single project based on id" do
     pending
-    project = @tm.project(1)
+    project = tm.project(1)
     project.should be_an_instance_of(@klass)
     project.name.should == 'project'
   end
 
   it "should be able to load a single project by attributes" do
     pending
-    project = @tm.project(:id => 1)
+    project = tm.project(:id => 1)
     project.should be_an_instance_of(@klass)
     project.name.should == 'project'
   end
